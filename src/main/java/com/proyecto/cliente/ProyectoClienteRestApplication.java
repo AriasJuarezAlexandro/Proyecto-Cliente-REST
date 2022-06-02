a1cb2477a32a005893a4d020dddf04288fe92842
AriasJuarezAlexandro <ariasjuarezalex@gmail.com>
2022-06-02 18:41:47 -0500

    inicio

---
+++ src/main/java/com/proyecto/cliente/ProyectoClienteRestApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.cliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoClienteRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoClienteRestApplication.class, args);
+	}
+
+}
